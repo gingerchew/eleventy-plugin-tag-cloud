6a0a6c4df3fdef1472f7fb9b3bbc099d56c22f6e
ginger <26461046+gingerchew@users.noreply.github.com>
2023-11-26 12:10:56 -0500

    Adds `tagCloudIndex` filter
    this filter exposes the quantity of posts with a certain tag
    
    changing the returned value from `tag:string` to `{ tagName: string, tagAmount: number }`

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,29 @@ module.exports = (eleventyConfig, _options) => {
     ...(_options?.ignore ?? [])
   ]
 
+  eleventyConfig.addFilter('tagCloudIndex', (posts = []) => {
+    if (!posts.length) throw new Error('[@tagCloudIndex]: Invalid collection passed, no items');
+
+    const tagMap = new Map();
+
+    for (const post of posts) {
+      const tags = post.data.tags;
+
+      tags?.forEach(tag => tagMap.set(tag, (tagMap.get(tag) || 0) + 1));
+    }
+
+    for (const _ignore of ignore) {
+      if (tagMap.has(_ignore)) tagMap.delete(_ignore);
+    }
+    
+    return [...tagMap.entries()].map(([key, value]) => {
+      return {
+        tagName: key,
+        tagAmount: value
+      }
+    })
+  })
+
   eleventyConfig.addFilter('tagCloud', (posts = []) => {
     if (!posts.length) throw new Error('[@tagCloud]: Invalid collection passed, no items');
 
@@ -16,9 +39,12 @@ module.exports = (eleventyConfig, _options) => {
 
     for (const post of posts) {
       const { tags } = post.data;
-      tags.forEach(tag => tagSet.add(tag));
+
+      tags.forEach(tag => {
+        tagSet.add(tag);
+      });
     }
-    
+
     for (const _ignore of ignore) {
       if (tagSet.has(_ignore)) tagSet.delete(_ignore);
     }
